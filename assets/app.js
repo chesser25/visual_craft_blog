e04599e903f1a0d5272175c44ee6abc79af4b785
Andrew <andrewchess25@gmail.com>
2021-04-11 15:20:41 +0300

    ADDED webpack, bootstrap, encore, auth.

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import './styles/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
+
+// start the Stimulus application
+import './bootstrap';
+
+bsCustomFileInput.init();
